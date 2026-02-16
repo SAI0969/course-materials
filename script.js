bb20ebb64451cb8ec1f8c595a7490ca890f32407
SAI0969 <asm1scs590526@gmail.com>
2026-02-16 17:38:31 +0530

    Update script.js

--- script.js
+++ script.js
@@ -8,3 +8,45 @@ function login() {
         document.getElementById("error").innerHTML = "Invalid Credentials!";
     }
 }
+
+function searchCourse() {
+    let input = document.getElementById("searchInput").value.toLowerCase();
+    let table = document.getElementById("resultTable");
+    let count = 0;
+
+    table.innerHTML = `
+        <tr>
+            <th>Semester</th>
+            <th>Course</th>
+            <th>Access</th>
+        </tr>
+    `;
+
+    courses.forEach(course => {
+        if (course.name.toLowerCase().includes(input)) {
+            count++;
+            table.innerHTML += `
+                <tr>
+                    <td>${course.sem}</td>
+                    <td>${course.name}</td>
+                    <td>
+                        <a href="${course.link}" target="_blank">
+                            <button class="open-btn">Open Material</button>
+                        </a>
+                    </td>
+                </tr>
+            `;
+        }
+    });
+
+    if (count === 0 && input !== "") {
+        table.innerHTML += `
+            <tr>
+                <td colspan="3">No course found</td>
+            </tr>
+        `;
+    }
+
+    document.getElementById("courseCount").innerText =
+        count > 0 ? count + " course(s) found" : "";
+}
